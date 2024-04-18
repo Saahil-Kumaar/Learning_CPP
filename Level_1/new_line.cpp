97dc2c80667ec9009c17a098dd78f73175190b79
Sahil Kumar <166407765+Saahil-Kumaar@users.noreply.github.com>
2024-04-18 22:38:52 +0530

    Add files via upload

---
+++ Level_1/new_line.cpp
@@ -0,0 +1,27 @@
+/* Usi\nn to change the line */
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"First Line<<endl";
+    cout<<"Second Line<<endl";
+    return 0;
+}
+*/
+
+/* Endl in series */
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"First Line"<<endl<<"Second Line"<<endl;
+    return 0;
+}
+/* Using \n like C-Programming */
+/*#include<iostream>
+using namespace std;
+int main(){
+    cout<<"First Line\n";
+    cout<<"Second Line\n";
+    return 0;
+}
+*/
