0abb16b6e0da46dc3cd39f499d0a73e35c0fd721
Sahil Kumar <166407765+Saahil-Kumaar@users.noreply.github.com>
2024-04-19 23:40:48 +0530

    Add files via upload

---
+++ Level_1/loops.cpp
@@ -0,0 +1,40 @@
+/* While loop */
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,i=0;
+//     cout<<"Enter last number: "<<endl;
+//     cin>>a;
+//     while(i<=a){
+//         cout<<i<<" ";   //to add space at end of each number.
+//         i=i+1;
+//     }
+// }
+
+/* Do-While Loop */
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,i=0;
+//     cout<<"Enter last number: "<<endl;
+//     cin>>a;
+//     do{
+//         cout<<i<<" ";   //to add space at end of each number.
+//         i=i+1;
+//     }while(i<=a);
+// }
+
+/* For Loop */
+#include<iostream>
+using namespace std;
+int main(){
+    int a,i=0;
+    cout<<"Enter last number: "<<endl;
+    cin>>a;
+    for (int i = 0; i <= a; i++)
+    {
+        cout<<i<<" ";
+    }
+    
+}
+
